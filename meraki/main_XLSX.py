33e4c201d6de83c8659fb61464457be439f51358
Jie Zhou <zhoujie@hawaii.edu>
2017-07-07 13:57:01 -1000

    Add files via upload

--- meraki/main_XLSX.py
+++ meraki/main_XLSX.py
@@ -21,19 +21,19 @@ class SwitchPort:
         self.name = row[3]
         self.tags = row[4]
 
-        if row[5].lower() == "true":
+        if row[5] == "TRUE":
             self.enabled = True
         else:
             self.enabled = False
 
-        if row[6].lower() == "true":
+        if row[6] == "TRUE":
             self.rstp = True
         else:
             self.rstp = False
 
         self.stp_guard = row[7]
 
-        if row[8].lower() == "true":
+        if row[8] == "TRUE":
             self.poe = True
         else:
             self.poe = False
@@ -94,16 +94,45 @@ def main():
     first_sheet = wb.get_sheet_names()[0]
     ws = wb[first_sheet]
 
-    my_row = []
-    for i in range(1, ws.max_row - 1 ):
-        for j in range(1, 14): # max column number
-            my_row.append(ws.cell(row=i, column=j).value)
-        configurations[ws.cell(row=i, column=2).value + str(ws.cell(row=i + 1, column=3).value)] = SwitchPort(my_row)  # dictionary
-        #print(ws.cell(row=i + 1, column=2).value + str(ws.cell(row=i + 1, column=3).value))
-        print(my_row)
-        my_row = []
+    ### valid row count ###
+    valid_row = 0 # will be incremented to the number of valid rows
+    threshold = 2 # how many consecutive blank rows are allow before program stops scanning excel content
+    real_row = 0 # track the actual row in excel file, including blank rows
+    th_count = threshold
+    while (th_count != 0):
+        real_row += 1
+        if(ws.cell(row = real_row, column = 2).value != None and ws.cell(row = real_row, column = 3).value != None):
+            # print(ws.cell(row = valid_row + 1, column=2).value)
+            valid_row += 1
+            th_count = threshold # if blank row is not consecutive to the number of threshold, reset th_count
+        elif(ws.cell(row = real_row, column = 2).value == None and ws.cell(row = real_row, column = 3).value != None):
+            print("row", real_row, "has incomplete entry")
+        elif (ws.cell(row = real_row, column = 2).value != None and ws.cell(row = real_row, column = 3).value == None):
+            print("row", real_row, "has incomplete entry")
+        else:
+            th_count -= 1
 
-    #print (configurations)
+    ### create dictionary for switch port ###
+    my_row = []
+    i = 0
+    while (valid_row != 0):
+        i += 1
+        if (ws.cell(row=i, column=2).value == None and ws.cell(row=i, column=3).value != None):
+            pass
+        elif (ws.cell(row=i, column=2).value != None and ws.cell(row=i, column=3).value == None):
+            pass
+        elif (ws.cell(row=i, column=2).value == None and ws.cell(row=i, column=3).value == None):
+            pass
+        else:
+            for j in range(1, 14):  # max column number
+                my_row.append(ws.cell(row=i, column=j).value)
+            configurations[ws.cell(row = i, column = 2).value , str(ws.cell(row = i, column = 3).value)] = SwitchPort(my_row)  # dictionary
+            # print(ws.cell(row = i, column = 2).value , str(ws.cell(row = i, column=3).value))
+            # print(my_row)
+            my_row = [] # reset
+            valid_row -= 1
+
+    # print (configurations)
 
     # API key.
     api_key = "8b43aaa7b92b6d3ad06234e6f581077620d3e512"
@@ -133,6 +162,8 @@ def main():
     for network in networks:
         devices += merakiapi.getnetworkdevices(api_key, network["id"], True)
 
+    # print(networks)
+
     # print devices
 
     switch_ports = []
@@ -150,25 +181,27 @@ def main():
         # Append the switch ports for the current switch to the master list.
         switch_ports += current_switch_ports
 
-    print (switch_ports)
+    # print (devices)
+    # print (switch_ports)
 
     # Apply configuration to the devices and push them to Meraki.
     for switch_port in switch_ports:
         try:
-            switch_port["name"] = configurations[switch_port["serial"]+str(switch_port["number"])].name
+            switch_port["name"] = configurations[switch_port["serial"],str(switch_port["number"])].name
         except:
             continue
-        switch_port["tags"]  = configurations[switch_port["serial"]+str(switch_port["number"])].tags
-        switch_port["enabled"] = configurations[switch_port["serial"]+str(switch_port["number"])].enabled
-        switch_port["rstpEnabled"] = configurations[switch_port["serial"]+str(switch_port["number"])].rstp
-        switch_port["stpGuard"] = configurations[switch_port["serial"]+str(switch_port["number"])].stp_guard
-        switch_port["poeEnabled"] = configurations[switch_port["serial"]+str(switch_port["number"])].poe
-        switch_port["type"] = configurations[switch_port["serial"]+str(switch_port["number"])].type
-        switch_port["vlan"] = configurations[switch_port["serial"]+str(switch_port["number"])].vlan
-        switch_port["voiceVlan"] = configurations[switch_port["serial"]+str(switch_port["number"])].voice_vlan
-        switch_port["allowedVlans"] = configurations[switch_port["serial"]+str(switch_port["number"])].allowed_vlan
+        switch_port["tags"]  = configurations[switch_port["serial"],str(switch_port["number"])].tags
+        switch_port["enabled"] = configurations[switch_port["serial"],str(switch_port["number"])].enabled
+        switch_port["rstpEnabled"] = configurations[switch_port["serial"],str(switch_port["number"])].rstp
+        switch_port["stpGuard"] = configurations[switch_port["serial"],str(switch_port["number"])].stp_guard
+        switch_port["poeEnabled"] = configurations[switch_port["serial"],str(switch_port["number"])].poe
+        switch_port["type"] = configurations[switch_port["serial"],str(switch_port["number"])].type
+        switch_port["vlan"] = configurations[switch_port["serial"],str(switch_port["number"])].vlan
+        switch_port["voiceVlan"] = configurations[switch_port["serial"],str(switch_port["number"])].voice_vlan
+        switch_port["allowedVlans"] = configurations[switch_port["serial"],str(switch_port["number"])].allowed_vlan
 
-        # print switch_port["enabled"]
+
+        print (switch_port["enabled"])
 
 
         merakiapi.updateswitchport(api_key, switch_port["serial"], switch_port["number"], switch_port["name"],
