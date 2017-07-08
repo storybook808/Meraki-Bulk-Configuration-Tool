b79e7ce667e3badc32eb85d7ffd56be9b095c0ef
wchong808 <wchong@hawaii.edu>
2017-07-07 14:47:25 -1000

    checking validation of xlsx

--- meraki/validate.py
+++ meraki/validate.py
@@ -2,27 +2,80 @@
 
 #this reads a xlsx and validates it according to the parameters of each field
 
-from openpyxl import workbook
 
 #field properties
-# Hostname: does not matter/any alphabet
-# Serial #: 12 alpha numeric string with/without dashes in the middle
-# Port #: just random number (i.e. 1-10)
-# Name: does not matter
-# Tags: does not matter
-# Enabled: True/False in any form (caps, lowercase, upercase)
-# RSTP: True/False in any form (caps, lowercase, upercase)
-# STP Guard: disabled/root gound/ BPDU
-# PoE: True/False in any form (caps, lowercase, upercase)
-# Type: access/ trunk in any form (caps, lowercase, upercase)
-# VLAN: just random number (i.e. 1-10)
-# Voice VLAN: just random number (i.e. 1-10)
-# Allowed VLANs: all/ comma seperated numbers (cannot read 000)
+# Hostname: does not matter/any alphabet col 0
+# Serial #: 12 alpha numeric string with/without dashes in the middle col 1
+# Port #: just random number (i.e. 1-10) col 2
+# Name: does not matter col 3
+# Tags: does not matter col 4
+# Enabled: True/False in any form (caps, lowercase, upercase) col 5
+# RSTP: True/False in any form (caps, lowercase, upercase) col 6
+# STP Guard: disabled/root gound/ BPDU col 7
+# PoE: True/False in any form (caps, lowercase, upercase) col 8
+# Type: access/ trunk in any form (caps, lowercase, upercase) col 9
+# VLAN: just random number (i.e. 1-10) col 10
+# Voice VLAN: just random number (i.e. 1-10) col 11
+# Allowed VLANs: all/ comma seperated numbers (cannot read 000) col 12
 
-# reading in xlsx
-wb = testing()
+import xlrd
+workbook = xlrd.open_workbook('testing.xlsx')
+worksheet = workbook.sheet_by_index(0)
 
-#grab the active worksheet
-ws = wb.active
+#for T/F values: Enabled, RSTP and PoE
+for row in range(1, worksheet.nrows):
+    # grab value for enable
+    j = worksheet.cell_value(row, 5)
+    # grab value for RSTP
+    k = worksheet.cell_value(row, 6)
+    # grab value for PoE
+    m = worksheet.cell_value(row, 8)
+    print (j, k, m)
 
-print(wb.active)
+    if j == 1 or j == 0 or j == '':
+        pass
+    else:
+        print('Enabled must be either True or False')
+        break
+    if k == 1 or k == 0 or k == '':
+        pass
+    else:
+        print('RSTP must be either True or False')
+        break
+    if m == 1 or m == 0 or m == '':
+        pass
+    else:
+        print('PoE must be either True or False')
+        break
+print('Enabled, RSTP and PoE is done')
+
+
+# for checking that serial # is a 12 alpha numberic string
+for row in range(1, worksheet.nrows):
+    l = worksheet.cell(row, 1)
+    print(l.value)
+
+
+
+# for checking that STP Guard must be 'disabled' 'root gound' or 'BPDU'
+for row in range(1, worksheet.nrows):
+    n = worksheet.cell(row, 7)
+    print(n.value.lower())
+    if n.value == "disabled" or n.value == 'root guard' or n.value == 'bpdu guard' or n.value == '':
+        print('this is right!')
+        pass
+    else:
+        print("""STP Guard must be 'disabled' 'Root guard' or 'BPDU guard'""")
+        break
+print('STP Guard is done')
+
+# for checking that Type is either access or trunk
+for row in range(1, worksheet.nrows):
+    o = worksheet.cell(row, 9)
+    print(o.value)
+    if o.value == "trunk" or o.value == "access" or o.value == '':
+        pass
+    else:
+        print("Type much be either access or trunk")
+        break
+print('Type is done')
